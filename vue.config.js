32f6b9456d5daeba0c95a776d9d951da5201e29e
wyh369352887 <18639003229@163.com>
2019-12-13 15:53:21 +0800

    update proxy config in vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
 module.exports = {
     publicPath: './',
     parallel: false,
+    devServer: {
+        proxy: {
+            '/api': {
+                target: 'http://uat-ngbx.55hudong.com',
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/api': ''
+                }
+            }
+        },
+    }
 }
